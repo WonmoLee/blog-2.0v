b442f910d1a4ba12f48a27e5b1cd248b0beb253e
unknown <sdafa@naver.com>
2020-06-10 17:27:38 +0900

    스크립트 공격막기

--- blog/src/com/cos/blog/util/HtmlParser.java
+++ blog/src/com/cos/blog/util/HtmlParser.java
@@ -6,6 +6,35 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 public class HtmlParser {
+	
+	public static String getContentYoutube(String content) {
+		Document doc = Jsoup.parse(content);
+		System.out.println(doc.toString());
+		Elements aTags = doc.select("a");
+		// https://youtu.be/TgOu00Mf3kI
+		// https://www.youtube.com/watch?v=yqtCGojXEpM
+		for (Element aTag : aTags) {
+			String href = aTag.attr("href");
+			String youtubeId = null;
+			System.out.println(href);
+			if(href != null && !aTag.attr("target").equals("_blank")) {
+				if(href.contains("https://youtu.be")) {
+					String[] hrefArr = href.split("/");
+					youtubeId = hrefArr[3];
+				}else if(href.contains("https://www.youtube.com")){
+					String[] hrefArr = href.split("=");
+					youtubeId = hrefArr[1];
+				}
+				
+				//System.out.println("JSOUP 파싱 : Youtube : "+youtubeId);
+				String video = "<br/><iframe src='http://www.youtube.com/embed/"+youtubeId+"' width='400px' height='400px' frameborder='0' allowfullscreen></iframe>";
+				//System.out.println("video : "+video);
+				aTag.after(video);
+			}
+		}
+		return doc.toString();
+	}
+	
 	public static String getContentPreview(String content) {
 		
 		Document doc = Jsoup.parse(content);
@@ -14,14 +43,14 @@ public class HtmlParser {
 		for (Element pTag : pTags) {
 			String text = pTag.text();
 			if(text.length() > 0) {
+				text = text.replaceAll("<", "&lt;").replaceAll(">", "&gt;");
 				if(text.length() < 11) {
-					return pTag.text();
+					return text;
 				}else {
-					return pTag.text().substring(0, 10)+"...";
+					return text.substring(0, 10)+"...";
 				}	
 			}
 		}
 		return "내용 없음...";
 	}
 }
-
